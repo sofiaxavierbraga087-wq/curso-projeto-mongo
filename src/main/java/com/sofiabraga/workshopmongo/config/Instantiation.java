aad7a4a893968a3e0020114a1560775f6102810f
Sofia Xavier <sofiaxavierbraga087@gmail.com>
2026-07-22 19:49:20 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/sofiabraga/workshopmongo/config/Instantiation.java
@@ -0,0 +1,35 @@
+package com.sofiabraga.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.sofiabraga.workshopmongo.domain.User;
+import com.sofiabraga.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	 
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("ENTREI NA CLASSE INSTANTIATION!");
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
+
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+	}
+
+}
